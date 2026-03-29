bce8190ef464237f6a480c36000395e846097b30
Kenshi Sabio <kenshisabio09@gmail.com>
2026-03-29 16:05:40 +0800

    Update Practice4.java

--- Java-Source-Base/src/Practice4.java
+++ Java-Source-Base/src/Practice4.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class Practice4 {
 
     // Insertion sort
+    // starts at index 1 by swapping index 0 and 1
 
     public static void insertionSort() {
         int[] arr = {5, 7, 1, 0, -2, 3}; // Array
@@ -12,14 +13,15 @@ public class Practice4 {
         System.out.print("Unsorted order: " + Arrays.toString(arr)); // Display original order
 
         for (int i = 1; i < n; i++) {
-            int temp = arr[i];
-            int j = i - 1;
+            int temp = arr[i]; // temp store 1 int per loop
+            int j = i - 1; // Starting point and current value for temp with elements on the left
 
+            // Compare elemnts
             while (j >= 0 && arr[j] > temp) { // > ascending order / < descending order
-                arr[j + 1] = arr[j];
-                j--;
+                arr[j + 1] = arr[j]; // shift greater number
+                j--; // move pointer to left element to compare
             }
-            arr[j + 1] = temp;
+            arr[j + 1] = temp; // insert temp
         }
         System.out.print("\nSorted order: " + Arrays.toString(arr)); // Display sorted order
     }
